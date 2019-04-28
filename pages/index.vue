7d7c1200bff2f981706fb86be272a6dcc17060b9
Henrik Stridsman <henrikstridsman@Henriks-MacBook-Pro.local>
2019-04-28 15:07:37 +0200

    style fixes general list

--- pages/index.vue
+++ pages/index.vue
@@ -8,15 +8,17 @@
         v-for="article in articlelist"
         :key="article.Id"
         class="uk-margin-small-bottom uk-link-reset">
-        <div class="uk-card uk-card-default">
+        <div class="uk-card uk-card-hover">
             <div class="uk-card-media-top">
                 <img :src="article.ImageIdThumb" alt="">
             </div>
-            <div class="uk-card-body uk-padding-small uk-text-small">
+            <div class="uk-card-body uk-padding-small uk-padding-remove-bottom uk-text-small">
               <strong>{{ article.HeadCategory }}</strong><br>
-              <span>{{ article.ArticleName }}</span><br>
-              <span>{{ article.PriceDisplay }}</span>
+              <span>{{ article.ArticleName }}</span>
             </div>
+            <div class="uk-card-footer uk-padding-small uk-padding-remove-top uk-text-small">
+              {{ article.PriceDisplay }}
+          </div>
         </div>
       </a>
     </div>
