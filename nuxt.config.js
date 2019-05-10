b606bf8805f5d2f34a8c79d1f14e0602662ac808
Erik Pettersson <petterson.erik@gmail.com>
2019-05-10 11:55:08 +0200

    Moved vars to it’s own file which can be imported from components

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,66 +1,65 @@
-import pkg from './package'
+import pkg from "./package";
+import * as path from "path";
 
 export default {
-  mode: 'universal',
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: false,
 
   /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/scss/site.scss'
-  ],
+   ** Global CSS
+   */
+  css: ["~/assets/scss/site.scss"],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    { src: '~/plugins/uikit.js', ssr: false }
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{ src: "~/plugins/uikit.js", ssr: false }],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    "@nuxtjs/axios"
   ],
   /*
-  ** Axios module configuration
-  */
- axios: {
-  // See https://github.com/nuxt-community/axios-module#options
-  baseURL: 'https://www.samdodds.com',
-  browserBaseURL: 'https://www.samdodds.com'
-},
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+    baseURL: "https://www.samdodds.com",
+    browserBaseURL: "https://www.samdodds.com"
+  },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     extend(config, ctx) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        scss: path.join(__dirname, "assets/scss")
+      };
     }
   }
-}
+};
