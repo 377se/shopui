4ba5b47798f39239b8b95a188940762ef47d9692
Erik Pettersson <petterson.erik@gmail.com>
2019-05-10 11:56:21 +0200

    Moved article rendering to component, added header

---
+++ components/articles/ArticleCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <a class="uk-margin-small-bottom uk-link-reset">
+    <div class="uk-card uk-card-hover bottom-red-line">
+      <div class="uk-card-media-top">
+        <img :src="article.ImageIdThumb" alt>
+      </div>
+      <div class="uk-card-body uk-padding-small uk-padding-remove-bottom uk-text-small">
+        <strong>{{ article.HeadCategory }}</strong>
+        <br>
+        <span>{{ article.ArticleName }}</span>
+      </div>
+      <div
+        class="uk-card-footer uk-padding-small uk-padding-remove-top uk-text-small"
+      >{{ article.PriceDisplay }}</div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    article: {}
+  },
+  mounted() {}
+};
+</script>
+
+<style lang="scss">
+@import "~scss/vars";
+
+.bottom-red-line {
+  border-bottom: 3px solid $global-primary-background;
+}
+</style>
