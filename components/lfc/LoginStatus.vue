8f96583e55394521806b53763d1f108b52bb2913
Erik Pettersson <petterson.erik@gmail.com>
2019-05-10 11:56:06 +0200

    Added Header component

---
+++ components/lfc/LoginStatus.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text" uk-text>LOGGA IN</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import "~scss/vars";
+
+.text {
+  color: $global-inverse-color;
+  margin-right: 18px;
+  font-size: 15px;
+}
+</style>
